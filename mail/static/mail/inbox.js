b443480e026f9911d1350d2c0e46d058d30d81ba
Peter Kuzmichev <petya.kuzmichev@gmail.com>
2022-02-28 10:23:25 +0300

    requests

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -44,14 +44,26 @@ function load_mailbox(mailbox) {
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
 
+  // document.querySelector('#mails-inbox').value = 'hahaha';
+  // document.getElementById('#mails-inbox').value = 'hah';
+
+
   // Get emails
   fetch('emails/inbox')
     .then(response => response.json())
     .then(emails => {
       console.log(emails)
+      console.log('lenght: ' + Object.keys(emails).length)
       // View for json on the page
-      document.querySelector('#mails-inbox').innerHTML = `${emails.slice(0)}`;
+      // for
+      for (let i = 0; i < Object.keys(emails).length; i++) {
+        console.log('keklol')
+        // json to html
+        document.querySelector('#mails-inbox').innerHTML = `<h1>heh ${emails[i]['subject']}</h1>`;
+      }
     })
+
+    // ${emails.slice(1)}
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
